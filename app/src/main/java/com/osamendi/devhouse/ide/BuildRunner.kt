1be9f9fd2c2cdaf26a876e6dbcb5c0aeebcbaa86
lolbd307-png <lolbd307@gmail.com>
2026-01-09 04:23:38 +0600

    Fix CI: remove invalid shell execution from Android app

--- app/src/main/java/com/osamendi/devhouse/ide/BuildRunner.kt
+++
@@ -1,9 +0,0 @@
-package com.osamendi.devhouse.ide
-
-import com.osamendi.devhouse.util.ShellExecutor
-
-object BuildRunner {
-    fun buildDebug(path: String) {
-        ShellExecutor.run("cd $path && ./gradlew assembleDebug")
-    }
-}
--- app/src/main/java/com/osamendi/devhouse/util/ShellExecutor.kt
+++
@@ -1,8 +0,0 @@
-package com.osamendi.devhouse.util
-
-object ShellExecutor {
-    fun run(cmd: String): String {
-        val process = Runtime.getRuntime().exec(cmd)
-        return process.inputStream.bufferedReader().readText()
-    }
-}
